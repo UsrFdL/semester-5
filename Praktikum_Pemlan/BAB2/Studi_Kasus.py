7ce0df0bc84c7d837957ffb5ed7afcfcd56f67bc
UsrFdL <fadhilaikhsani@yahoo.com>
2022-09-15 12:35:01 +0700

    .

---
+++ Praktikum_Pemlan/BAB2/Studi_Kasus.py
@@ -0,0 +1,8 @@
+A = 10
+B = 20
+C = (A*2) + (B*2) + (A*B)
+
+dec = float(C)
+itg = int(C)
+
+print('1. {} ({})\n2. {} ({})'.format(dec, type(dec), itg, type(itg)))
---
+++ Praktikum_Pemlan/BAB2/Tugas2.py
@@ -0,0 +1,10 @@
+"""
+Buatlah program sederhana seputar bab yang disampaikan. Yang harus dikumpulkan
+- Judul Program
+- Latar Belakang pembuatan program (jelaskan secara singkat)
+- Kode Program
+- Penjelasan per baris
+- Screenshoot program (ada unsur nama sendiri)
+- Screen Shoot Output
+"""
+
